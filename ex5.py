ee20378664823502afa864949ede0da766b90d1b
Tony Ramirez <tony.ramirez@motionloft.com>
2012-04-07 16:16:31 -0700

    Kg and lbs

--- ex5.py
+++ ex5.py
@@ -5,10 +5,15 @@ weight = 175 #lbs
 eyes = 'brown' 
 teeth = 'coffee'
 hair = 'Brown'
+centimeters = height * 2.54
+kilograms = weight / 2.2
+
 
 print "Let's talk about %r." % name
 print "He's %d inches tall." % height
+print "He's %d centimeters tall though" % centimeters
 print "He's %d pounds heavy" % weight
+print "He's %d heavy in kg's" % kilograms
 print "Actually that's not too heavy."
 print "He's got %r eyes and %r hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
