1be68aea9bcef66ceabb23e54953d6b28fc47291
Tony Ramirez <tony.ramirez@motionloft.com>
2012-04-07 16:09:08 -0700

    ex 5 updated

--- ex5.py
+++ ex5.py
@@ -1,11 +1,20 @@
-my_name = 'Tony Ramirez'
-my_age = 23 # not a lie
-my_height = 70 # inches
-my_weight = 175 #lbs
-my_eyes = 'brown' 
-my_teeth = 'coffee'
-my_hair = 'Brown'
+name = 'Tony'
+age = 23 # not a lie
+height = 70 # inches
+weight = 175 #lbs
+eyes = 'brown' 
+teeth = 'coffee'
+hair = 'Brown'
 
-print 
+print "Let's talk about %r." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy" % weight
+print "Actually that's not too heavy."
+print "He's got %r eyes and %r hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
+print "If I add %d, %d, and %d I get %d" % (
+       age, height, weight, age + height + weight)
 
+#It matters what character you use after the percent sign the letters found here http://docs.python.org/library/stdtypes.html#string-formatting
+#are meant to define how to convert the bject or variable you defined string or umber or whatever
