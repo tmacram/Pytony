52ae832eb9776c82513f5ea78b77a9c194ccee83
Tony Ramirez <tony.ramirez@motionloft.com>
2012-04-07 21:40:03 -0700

    Python exercises through 15

---
+++ ex11Asking_Questions.py
@@ -0,0 +1,17 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How muh do you weigh?"
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+print "Wher do you get off?"
+answer = raw_input()
+print"Is that a fact?"
+answer2 = raw_input()
+
+
+print "Alright sayonara biaotch!!!!"
+
---
+++ ex13parameter_Unpacking_variables.py
@@ -0,0 +1,8 @@
+from sys import argv 
+
+script, cheese, apple, bread = argv
+
+print "The script is called:", script
+print "Your first variable is:", cheese
+print "Your second variable is:", apple
+print 'Your third variable is:', bread
